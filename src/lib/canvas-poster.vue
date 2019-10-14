096a385c04ba64c2b00a93255d0cc88f6a8a805b
feiniu <tao.liu@epmap.org>
2019-10-14 17:20:13 +0800

    feat: 字符串中增加<blod></blod>用来标记加粗文本
    在字符串中可以用<blod></blod>包住需要加粗的文本。英语需要和单词之间保持一个空格的距离

--- src/lib/canvas-poster.vue
+++ src/lib/canvas-poster.vue
@@ -4,9 +4,9 @@
   </div>
 </template>
 <script>
-const QR = require('./qrcode.js');
+const QR = require("./qrcode.js");
 export default {
-  name: 'CanvasPoster',
+  name: "CanvasPoster",
   components: {},
   props: {
     painting: {
@@ -14,14 +14,13 @@ export default {
       value: {}
     }
   },
-  computed: {
-  },
+  computed: {},
   watch: {
     painting: {
-      handler: 'handlePaint',
+      handler: "handlePaint",
       deep: true,
       immediate: true
-    },
+    }
   },
   data() {
     return {
@@ -32,271 +31,464 @@ export default {
       isPainting: false,
       canvas: null,
       ctx: null
-    }
+    };
   },
 
   mounted() {
     this.$nextTick(() => {
-      this.canvas = this.$refs.canvas // 指定canvas
-      this.ctx = this.canvas.getContext("2d")//设置2D渲染区域
-    })
+      this.canvas = this.$refs.canvas; // 指定canvas
+      this.ctx = this.canvas.getContext("2d"); //设置2D渲染区域
+    });
   },
   methods: {
+    // 把字符串切割为数组，<> 括住的为一个字符
+    splitTag(str) {
+      const tokens = [];
+      for (let i = 0; i < str.length; i++) {
+        if (
+          (str[i] === "<" && str[i + 1] === "b" && str[i + 2] === "l") ||
+          (str[i] === "<" && str[i + 1] === "/" && str[i + 2] === "b")
+        ) {
+          let tag = "";
+          while (str[i] !== ">") {
+            tag += str[i];
+            i++;
+          }
+          tag += str[i];
+          tokens.push(tag);
+        } else {
+          tokens.push(str[i]);
+        }
+      }
+
+      return tokens;
+    },
+    // 数组转字符串，并且去掉不需要的字符
+    removeListWord(wordList) {
+      let _tempStrList = [];
+      for (let i = 0; i < wordList.length; i++) {
+        if (wordList[i] !== "<blod>" && wordList[i] !== "</blod>") {
+          _tempStrList.push(wordList[i]);
+        }
+      }
+      return _tempStrList.join("");
+    },
     // 开始绘图
     handlePaint(newVal, oldVal) {
       if (newVal.views && newVal !== oldVal) {
-        const { width, height, views } = this.painting
-        this.width = width
-        this.height = height
+        const { width, height, views } = this.painting;
+        this.width = width;
+        this.height = height;
         const inter = setInterval(() => {
           if (this.ctx) {
-            clearInterval(inter)
+            clearInterval(inter);
             // 重新绘图
-            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-            this.ctx.save()
-            this.getImages(views)
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.save();
+            this.getImages(views);
           }
-        }, 100)
+        }, 100);
       }
     },
     //加载图片
     loadImage(url) {
       return new Promise((resolve, reject) => {
-        const img = new Image()
-        img.setAttribute('crossorigin', 'anonymous');
-        img.onload = () => resolve(img)
-        img.onerror = () => reject(`下载图片失败 ${url}`)
-        img.src = url
-      })
+        const img = new Image();
+        img.setAttribute("crossorigin", "anonymous");
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(`下载图片失败 ${url}`);
+        img.src = url;
+      });
     },
     // 加载图片
     getImages(views) {
-      const imageList = []
+      const imageList = [];
       for (let i = 0; i < views.length; i++) {
-        if (views[i].type === 'image') {
-          imageList.push(this.loadImage(views[i].url))
+        if (views[i].type === "image") {
+          imageList.push(this.loadImage(views[i].url));
         }
       }
-      Promise.all(imageList).then(res => {
-        this.imageList = res
-        this.startPainting()
-      }).catch((err) => {
-        this.$emit('fail', err)
-      })
+      Promise.all(imageList)
+        .then(res => {
+          this.imageList = res;
+          this.startPainting();
+        })
+        .catch(err => {
+          this.$emit("fail", err);
+        });
     },
     startPainting() {
-      const { views } = this.painting
+      const { views } = this.painting;
       for (let i = 0, imageIndex = 0; i < views.length; i++) {
-        if (views[i].type === 'image') {
+        if (views[i].type === "image") {
           this.drawImage({
             ...views[i],
             img: this.imageList[imageIndex]
-          })
-          imageIndex++
-        } else if (views[i].type === 'text') {
-          this.drawText(views[i])
-        } else if (views[i].type === 'rect') {
-          this.drawRect(views[i])
-        } else if (views[i].type === 'qrcode') {
-          this.drawQRCode(views[i])
+          });
+          imageIndex++;
+        } else if (views[i].type === "text") {
+          this.drawText(views[i]);
+        } else if (views[i].type === "rect") {
+          this.drawRect(views[i]);
+        } else if (views[i].type === "qrcode") {
+          this.drawQRCode(views[i]);
         }
       }
-      var imageBase64 = this.canvas.toDataURL("image/png")
-      this.$emit('success', imageBase64)
-
+      var imageBase64 = this.canvas.toDataURL("image/png");
+      this.$emit("success", imageBase64);
     },
     /**
-      * 根据 radius 进行裁减
+     * 根据 radius 进行裁减
      */
     _doClip(left, top, width, height, radius) {
-      this.ctx.beginPath()
-      // 左上角 
-      this.ctx.arc(left + radius, top + radius, radius, Math.PI, Math.PI * 1.5)
+      this.ctx.beginPath();
+      // 左上角
+      this.ctx.arc(left + radius, top + radius, radius, Math.PI, Math.PI * 1.5);
       // border-top
-      this.ctx.moveTo(left + radius, top)
-      this.ctx.lineTo(left + width - radius, top)
-      this.ctx.lineTo(left + width, top + radius)
+      this.ctx.moveTo(left + radius, top);
+      this.ctx.lineTo(left + width - radius, top);
+      this.ctx.lineTo(left + width, top + radius);
       // 右上角
-      this.ctx.arc(left + width - radius, top + radius, radius, Math.PI * 1.5, Math.PI * 2)
-      // border-right 
-      this.ctx.lineTo(left + width, top + height - radius)
-      this.ctx.lineTo(left + width - radius, top + height)
-      // 右下角 
-      this.ctx.arc(left + width - radius, top + height - radius, radius, 0, Math.PI * 0.5)
+      this.ctx.arc(
+        left + width - radius,
+        top + radius,
+        radius,
+        Math.PI * 1.5,
+        Math.PI * 2
+      );
+      // border-right
+      this.ctx.lineTo(left + width, top + height - radius);
+      this.ctx.lineTo(left + width - radius, top + height);
+      // 右下角
+      this.ctx.arc(
+        left + width - radius,
+        top + height - radius,
+        radius,
+        0,
+        Math.PI * 0.5
+      );
       // border-bottom
-      this.ctx.lineTo(left + radius, top + height); this.ctx.lineTo(left, top + height - radius)
-      // 左下角 
-      this.ctx.arc(left + radius, top + height - radius, radius, Math.PI * 0.5, Math.PI)
+      this.ctx.lineTo(left + radius, top + height);
+      this.ctx.lineTo(left, top + height - radius);
+      // 左下角
+      this.ctx.arc(
+        left + radius,
+        top + height - radius,
+        radius,
+        Math.PI * 0.5,
+        Math.PI
+      );
       // border-left
       this.ctx.lineTo(left, top + radius);
-      this.ctx.lineTo(left + radius, top)
+      this.ctx.lineTo(left + radius, top);
       // 这里是使用 fill 还是 stroke都可以，二选一即可，但是需要与上面对应
-      this.ctx.fill()
-      this.ctx.closePath()
-
+      this.ctx.fill();
+      this.ctx.closePath();
     },
     _doRoate(left, top, height, width, deg) {
-      this.ctx.translate(left + width / 2, top + height / 2)
-      this.ctx.rotate(deg * Math.PI / 180)
+      this.ctx.translate(left + width / 2, top + height / 2);
+      this.ctx.rotate((deg * Math.PI) / 180);
     },
     drawImage(params) {
-      this.ctx.save()
-      const { img, top = 0, left = 0, width = 0, height = 0, radius = 0, deg = 0, opacity=1 } = params
-      this.ctx.globalAlpha = opacity      
+      this.ctx.save();
+      const {
+        img,
+        top = 0,
+        left = 0,
+        width = 0,
+        height = 0,
+        radius = 0,
+        deg = 0,
+        opacity = 1
+      } = params;
+      this.ctx.globalAlpha = opacity;
       if (radius) {
-        this._doClip(left, top, width, height, radius)
-        this.ctx.clip()
-        this.ctx.drawImage(img, left, top, width, height)
+        this._doClip(left, top, width, height, radius);
+        this.ctx.clip();
+        this.ctx.drawImage(img, left, top, width, height);
       } else {
         if (deg !== 0) {
-          this._doRoate(left, top, height, width, deg)
-          this.ctx.drawImage(img, -width / 2, -height / 2, width, height)
+          this._doRoate(left, top, height, width, deg);
+          this.ctx.drawImage(img, -width / 2, -height / 2, width, height);
         } else {
-          this.ctx.drawImage(img, left, top, width, height)
+          this.ctx.drawImage(img, left, top, width, height);
         }
       }
-      this.ctx.restore()
+      this.ctx.restore();
     },
     drawQRCode(params) {
-      this.ctx.save()
-      const { width = 0, height = 0, left = 0, top = 0, content, background, color } = params
-      QR.api.draw(content, this.ctx, left, top, width, height, background, color)
-      this.ctx.restore()
+      this.ctx.save();
+      const {
+        width = 0,
+        height = 0,
+        left = 0,
+        top = 0,
+        content,
+        background,
+        color
+      } = params;
+      QR.api.draw(
+        content,
+        this.ctx,
+        left,
+        top,
+        width,
+        height,
+        background,
+        color
+      );
+      this.ctx.restore();
     },
 
     // 写字
     drawText(params) {
-      this.ctx.save()
+      const that = this;
+      that.ctx.save();
       let {
         MaxLineNumber = 2,
         breakWord = false,
-        color = 'black',
-        content = '',
+        color = "black",
+        content = "",
         fontSize = 16,
         top = 0,
         left = 0,
         lineHeight = 20,
-        textAlign = 'left',
+        textAlign = "left",
         width,
         bolder = false,
-        textDecoration = 'none',
+        textDecoration = "none",
         isEnglish = false
-      } = params
+      } = params;
 
-      this.ctx.beginPath()
+      that.ctx.beginPath();
       // this.ctx.textBaseline = 'top'
-      this.ctx.textBaseline = "Bottom"
-      this.ctx.textAlign = textAlign
-      this.ctx.fillStyle = color
+      that.ctx.textBaseline = "Bottom";
+      that.ctx.textAlign = textAlign;
+      that.ctx.fillStyle = color;
       //  this.ctx.font = "normal 36px Arial";
-      this.ctx.font = `normal ${fontSize}px Arial`;
+      that.ctx.font = `normal normal ${fontSize}px Arial`;
       //  top + fontSize  解决 this.ctx.textBaseline = 'top'问题
-      const textTop = top + fontSize
+      const textTop = top + fontSize;
+
       if (!breakWord) {
-        this.ctx.fillText(content, left, textTop)
-        this.drawTextLine(left, textTop, textDecoration, color, fontSize, content)
+        that.ctx.fillText(content, left, textTop);
+        that.drawTextLine(
+          left,
+          textTop,
+          textDecoration,
+          color,
+          fontSize,
+          content
+        );
       } else {
-        let fillText = ''
-        let fillTop = textTop
-        let lineNum = 1
+        let fillText = [];
+        let fillTop = textTop;
+        let lineNum = 1;
         if (isEnglish) {
-          let _tempContent = []
-          content = content.split(' ')
+          let _tempContent = [];
+          content = content.split(" ");
           for (let i = 0; i < content.length; i++) {
-            _tempContent.push(content[i])
-            _tempContent.push(" ")
+            _tempContent.push(content[i]);
+            _tempContent.push(" ");
           }
-          content = _tempContent
+          content = _tempContent;
+        } else {
+          content = that.splitTag(content);
         }
 
         // 临时变量，保存超过长度的最后一个字符
-        let _tempWord = ""
+        let _tempWord = "";
 
         for (let i = 0; i < content.length; i++) {
-          if (!fillText) {
-            fillText += [_tempWord]
+          if (fillText.length === 0) {
+            if (_tempWord !== "" && _tempWord !== undefined) {
+              fillText.push(_tempWord);
+            }
           }
-          // fillText += [_tempWord]
-          if (this.ctx.measureText(fillText + [content[i]]).width > width) {
-            _tempWord = content[i]
+          // TODO 这里需要去除<blod>和</blod>所占据的长度，因为这些不会显示，只是一个标记
+          let _tempText = that.removeListWord(fillText);
+
+          if (that.ctx.measureText(_tempText + [content[i]]).width > width) {
+            _tempWord = content[i];
             if (lineNum === MaxLineNumber) {
               if (i !== content.length) {
-                fillText = fillText.substring(0, fillText.length - 1) + '...'
-                this.ctx.fillText(fillText, left, fillTop)
-                this.drawTextLine(left, fillTop, textDecoration, color, fontSize, fillText)
-                fillText = ''
-                break
+                fillText.pop();
+                fillText.push["..."];
+                that.ctx.fillText(fillText.join(""), left, fillTop);
+                that.drawTextLine(
+                  left,
+                  fillTop,
+                  textDecoration,
+                  color,
+                  fontSize,
+                  _tempText
+                );
+                fillText = "";
+                break;
               }
             }
-            this.ctx.fillText(fillText, left, fillTop)
-            this.drawTextLine(left, fillTop, textDecoration, color, fontSize, fillText)
-            fillText = ''
-            fillTop += lineHeight
-            lineNum++
+
+            // 需要绘制的文字列表，这里面没有<blod>标签
+            let tempDrawText = [];
+
+            // 当前 x轴位置
+            let xCoordinate = left;
+
+            for (let i = 0; i < fillText.length; i++) {
+              if (fillText[i] === "<blod>" || fillText[i] === "</blod>") {
+                if (tempDrawText.length === 0) {
+                  if (fillText[i] === "<blod>") {
+                    that.ctx.font = `normal bolder ${fontSize}px Arial`;
+                  }
+                  if (fillText[i] === "</blod>") {
+                    that.ctx.font = `normal normal ${fontSize}px Arial`;
+                  }
+                  continue;
+                }
+
+                that.ctx.fillText(tempDrawText.join(""), xCoordinate, fillTop);
+                xCoordinate += that.ctx.measureText(tempDrawText.join(""))
+                  .width;
+                tempDrawText = [];
+
+                if (fillText[i] === "<blod>") {
+                  that.ctx.font = `normal bolder ${fontSize}px Arial`;
+                }
+                if (fillText[i] === "</blod>") {
+                  that.ctx.font = `normal normal ${fontSize}px Arial`;
+                }
+                continue;
+              }
+              tempDrawText.push(fillText[i]);
+            }
+
+            if (tempDrawText.length > 0) {
+              that.ctx.fillText(tempDrawText.join(""), xCoordinate, fillTop);
+            }
+            that.drawTextLine(
+              left,
+              fillTop,
+              textDecoration,
+              color,
+              fontSize,
+              _tempText
+            );
+            fillText = [];
+            fillTop += lineHeight;
+            lineNum++;
           } else {
-            fillText += [content[i]]
+            fillText.push(content[i]);
           }
         }
-        this.ctx.fillText(fillText, left, fillTop)
-        this.drawTextLine(left, fillTop, textDecoration, color, fontSize, fillText)
+
+        // 长度未超过规定长度
+        // 需要绘制的文字列表，这里面没有<blod>标签
+        let tempDrawText = [];
+
+        // 当前 x轴位置
+        let xCoordinate = left;
+
+        for (let i = 0; i < fillText.length; i++) {
+          if (fillText[i] === "<blod>" || fillText[i] === "</blod>") {
+            if (tempDrawText.length === 0) {
+              if (fillText[i] === "<blod>") {
+                that.ctx.font = `normal bolder ${fontSize}px Arial`;
+              }
+              if (fillText[i] === "</blod>") {
+                that.ctx.font = `normal normal ${fontSize}px Arial`;
+              }
+              continue;
+            }
+
+            that.ctx.fillText(tempDrawText.join(""), xCoordinate, fillTop);
+            xCoordinate += that.ctx.measureText(tempDrawText.join("")).width;
+            tempDrawText = [];
+
+            if (fillText[i] === "<blod>") {
+              that.ctx.font = `normal bolder ${fontSize}px Arial`;
+            }
+            if (fillText[i] === "</blod>") {
+              that.ctx.font = `normal normal ${fontSize}px Arial`;
+            }
+            continue;
+          }
+          tempDrawText.push(fillText[i]);
+        }
+        if (tempDrawText.length > 0) {
+          that.ctx.fillText(tempDrawText.join(""), xCoordinate, fillTop);
+        }
+        // TODO 这条代码不知是否需要！
+        that.drawTextLine(
+          left,
+          fillTop,
+          textDecoration,
+          color,
+          fontSize,
+          fillText
+        );
       }
-      this.ctx.restore()
+      that.ctx.restore();
       if (bolder) {
-        this.drawText({
+        that.drawText({
           ...params,
           left: left + 0.3,
           top: top + 0.3,
           bolder: false,
-          textDecoration: 'none'
-        })
+          textDecoration: "none"
+        });
       }
     },
     // 画直线
     drawTextLine(left, top, textDecoration, color, fontSize, content) {
-      if (textDecoration === 'underline') {
+      if (textDecoration === "underline") {
         this.drawRect({
           background: color,
           top: top,
           left: left - 1,
           width: this.ctx.measureText(content).width + 3,
           height: 1
-        })
-      } else if (textDecoration === 'line-through') {
+        });
+      } else if (textDecoration === "line-through") {
         this.drawRect({
           background: color,
           top: top - fontSize / 3,
           left: left - 1,
           width: this.ctx.measureText(content).width + 3,
           height: 1
-        })
-
+        });
       }
     },
     _getAngle(angle) {
-      return Number(angle) * Math.PI / 180;
+      return (Number(angle) * Math.PI) / 180;
     },
     // 画矩形
     drawRect(params) {
-      this.ctx.save()
-      const { background = 'white', top = 0, left = 0, width = 0, height = 0, radius = 0, deg = 0 } = params
-      this.ctx.fillStyle = background
+      this.ctx.save();
+      const {
+        background = "white",
+        top = 0,
+        left = 0,
+        width = 0,
+        height = 0,
+        radius = 0,
+        deg = 0
+      } = params;
+      this.ctx.fillStyle = background;
       if (radius) {
-        this._doClip(left, top, width, height, radius)
+        this._doClip(left, top, width, height, radius);
       } else {
         if (deg !== 0) {
-          this._doRoate(left, top, height, width, deg)
-          this.ctx.fillRect(-width / 2, -height / 2, width, height)
+          this._doRoate(left, top, height, width, deg);
+          this.ctx.fillRect(-width / 2, -height / 2, width, height);
         } else {
-          this.ctx.fillRect(left, top, width, height)
+          this.ctx.fillRect(left, top, width, height);
         }
         // this.ctx.fillRect(left, top, width, height)
       }
-      this.ctx.restore()
-    },
-
+      this.ctx.restore();
+    }
   }
-}
-
+};
 </script>
 <style  scoped>
 .canvas {
